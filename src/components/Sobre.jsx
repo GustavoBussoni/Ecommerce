97d2ceecde1b17c44401a996957ed6adb905421a
GustavoBussoni <04gubussoni@gmail.com>
2025-08-22 14:04:27 -0300

    descrição do que mudou

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -4,7 +4,7 @@ const Sobre = () => {
   return (
     <section className="sobre">
       <div className="container">
-        <h2>Sobre a Nossa Marca</h2>
+        <h2>SOBRE A NEW YORK</h2>
         <p>
           Descubra as últimas tendências em roupas, calçados, bolsas e acessórios masculinos e femininos da estação. Aposte em um visual clássico e casual combinando camisa social e jeans ou, se preferir um look mais descontraído, invista em camisetas estilosas. Complete o visual com cintos, tênis e outros acessórios que elevam qualquer produção. Para elas, a coleção feminina traz elegância e versatilidade, perfeita para sair à noite, trabalhar ou aproveitar o dia a dia com conforto e estilo.
         </p>
